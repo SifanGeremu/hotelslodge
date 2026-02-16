332bc5e8faea6d449460870b9a54026d8e480a28
SifanGeremu <wins33975@gmail.com>
2026-02-16 14:33:35 +0300

    feat(models): add Booking and Guest models

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Booking extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $primaryKey = 'booking_id';
+       protected $fillable = [
+'hotel_id',	
+'guest_id',	
+'room_id',	
+'checkin_date',	
+'checkout_date',	
+'created_at',	
+'updated_at',	
+    ];
+    public $timestamps = false;
 }
--- app/Models/Guest.php
+++ app/Models/Guest.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Guest extends Model
 {
-    //
+    
+      use HasFactory;
+    
+    protected $primaryKey = 'guest_id';
+       protected $fillable = [
+'hotel_id',	
+'first_name',
+'last_name',
+'email',
+'phone_number',
+'checkin_date',	
+'checkout_date',	
+    ];
+    public $timestamps = false;
 }
